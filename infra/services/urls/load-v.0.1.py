fa3927b7bc41cb166fbaae3f8d8eac3585f3aefa
nik786 <chakraborty.rock@gmail.com>
2020-04-01 19:45:10 +0530

    Create load-v.0.1.py

---
+++ infra/services/urls/load-v.0.1.py
@@ -0,0 +1,12 @@
+import requests
+import argparse
+
+
+def check_url():
+    url = "http://localhost:75"
+    for i in range(1,100):
+        r=requests.get(url)
+        #print(r)
+        print("HTTP/1.1 %s %s"%(r.status_code,r.reason))
+
+check_url()
