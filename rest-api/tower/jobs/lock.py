a321d06b4839ffa1ac269824aabef2361a3b7841
nik786 <chakraborty.rock@gmail.com>
2020-05-03 19:15:54 +0530

    Create lock.py

---
+++ rest-api/tower/jobs/lock.py
@@ -0,0 +1,8 @@
+def lockdown(item):
+  print("\033[91m\033[4mLOCKDOWN:\033[0m")  
+  for x in item:
+    print(x)
+  else:
+    print("\033[91mHAPPY QURANTINED!\033[0m")  
+
+list = ["stay home", "no sport", "no beer", "no hangout with gf"]
