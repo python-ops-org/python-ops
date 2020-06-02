55b293dd9d191ffe334c576941bd0de00a01d8ea
nik786 <chakraborty.rock@gmail.com>
2020-06-02 23:38:54 +0530

    Create art-18.py

---
+++ rest-api/tower/jobs/art-18.py
@@ -0,0 +1,47 @@
+import requests
+import argparse
+import json
+import csv
+from bs4 import BeautifulSoup
+import re
+
+#span class="ansi33"
+
+token = "b6aCCurUXr6nAAb2JtPaxV3KvK1Lct"
+headers = {"Content-Type": "application/json", "Authorization": "Bearer %s"%token}
+
+def work():
+	writing= open('output.csv', "w+")
+	writer=csv.writer(writing)
+	url = "http://192.168.56.159"
+	next = "/api/v2/jobs/"
+	print "job_id\turl"
+	writer.writerow(["job_id","url"])
+	while True:
+		req = requests.get("%s%s"%(url,next),headers=headers)
+		data = json.loads(req.text)
+		ids = []
+		for job in data["results"]:
+			ids.append(job["id"])
+		for id in ids:
+			urls = ["/job_events/?search=war","/job_events/?search=jar","/job_events/?search=ear"]
+			name = ""
+			for file_url in urls:
+				req_2 = requests.get("%s/api/v2/jobs/%s%s"%(url,id,file_url),headers=headers)
+				data_2 = json.loads(req_2.text,strict=False)
+				for trial in data_2["results"]:
+					try:
+						name = trial["event_data"]["res"]["url"]
+					except:
+						continue
+			if name != "":
+				print "%s\t%s"%(id,name)
+				writer.writerow([id,name])
+			else:
+				print "%s\tnot present"%id
+				writer.writerow([id,"not present"])
+		if data["next"] == None:
+			break
+		else:
+			next = data["next"]
+work()
