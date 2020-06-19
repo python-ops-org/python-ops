ebc4872e15635d4fcbc9ed0c99747a259923b924
nik786 <chakraborty.rock@gmail.com>
2020-06-19 16:13:12 +0530

    Update art-12.py

--- rest-api/tower/jobs/art-12.py
+++ rest-api/tower/jobs/art-12.py
@@ -15,15 +15,19 @@ def work():
 	writer=csv.writer(writing)
 	url = "http://192.168.56.159"
 	next = "/api/v2/jobs/"
-	print "job_id\turl"
-	writer.writerow(["job_id","url"])
+	print("job_id\turl\t\ttemplate_id\tcr\tem")
+	writer.writerow(["job_id","url","template_id","cr","em"])
 	while True:
 		req = requests.get("%s%s"%(url,next),headers=headers)
 		data = json.loads(req.text)
-		ids = []
+		ids_tmp = []
 		for job in data["results"]:
-			ids.append(job["id"])
-		for id in ids:
+			ids_tmp.append([job["id"],job["summary_fields"]["job_template"]["id"]])
+		for element in ids_tmp:
+			cr = "not available"
+			em = "not available"
+			id = element[0]
+			template = element[1]
 			urls = ["/job_events/?search=war","/job_events/?search=jar","/job_events/?search=ear"]
 			name = ""
 			for file_url in urls:
@@ -35,15 +39,38 @@ def work():
 					except:
 						continue
 			if name != "":
-				print "%s\t%s"%(id,name)
-				patch = {"name": "Demo Job Template","description": "","job_type": "run","extra_vars": "nexus_url: %s"%name}
-				requests.patch("http://192.168.56.159/api/v2/job_templates/7/",json=patch,headers=headers)
-				writer.writerow([id,name])
+				req_3 = requests.get("%s/api/v2/job_templates/%s/"%(url,template),headers=headers)
+				tmp_data = json.loads(req_3.text)
+				cr = tmp_data["extra_vars"].split("\n")[-2].split(" ")[1].replace('"',"")
+				em = tmp_data["extra_vars"].split("\n")[-1].split(" ")[1].replace('"',"")
+				print("%s\t%s\t%s\t%s\t%s"%(id,name,template,cr,em))
+				writer.writerow([id,name,template,cr,em])
 			else:
-				print "%s\tnot present"%id
-				writer.writerow([id,"not present"])
+				print("%s\t%s\t%s\t%s\t%s"%(id,name,template,cr,em))
+				writer.writerow([id,name,template,cr,em])
 		if data["next"] == None:
 			break
 		else:
 			next = data["next"]
+	writing.close()
+	fo = open("output.csv","r")
+	for line in fo:
+		line = line.split(",")
+		id = line[0]
+		url = line[1]
+		template = line[2]
+		cr = line[3]
+		em = line[4]
+		if (".war" in url) or ("jar" in url) or (".ear" in url):
+			if (cr != "not available") and (em != "not available"):
+						patch1 = {"name": "get_url_test_02","description": '',"job_type": "run","verbosity": cr,"forks": em, "nexus_url": url}
+						tmp1 = requests.patch("http://192.168.56.159/api/v2/job_templates/17/",json=patch1,headers=headers)
+						data_patch1 = json.loads(tmp1.text)
+						ver = data_patch1["verbosity"]
+						if tmp1.status_code == 200:
+							patch2 = {"name": "get_url_test_03","description": '',"job_type": "run","verbosity": ver,"forks": em }
+							tmp2 = requests.patch("http://192.168.56.159/api/v2/job_templates/18/",json=patch2,headers=headers)
+							print(tmp2.text)
+			else:
+					continue
 work()
