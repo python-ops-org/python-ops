6bffe2802766e44e52763f766e97002fdec807eb
nik786 <chakraborty.rock@gmail.com>
2020-06-28 20:26:07 +0530

    Create dynamodb-insert.py

---
+++ aws/storage/lambda-/s3/dynamodb-insert.py
@@ -0,0 +1,14 @@
+import boto3
+import json
+
+d = boto3.resource("dynamodb")
+t = d.Table("library")
+t.put_item (
+        Item = {
+            "books": "3",
+            "author": "rowling",
+            "title": "hogwart"
+
+            }
+
+)
