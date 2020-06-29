5bd478c0bcfd22539e79f163414a0f6ecffd20bc
nik786 <chakraborty.rock@gmail.com>
2020-06-29 14:27:40 +0530

    Update fact.py

--- KT/murthy/exercise-8-8-18/fact.py
+++ KT/murthy/exercise-8-8-18/fact.py
@@ -1,3 +1,5 @@
+
+"""
 def factorial(n):
     if n == 0:
         return 1
@@ -5,3 +7,13 @@ def factorial(n):
         return n * factorial(n-1)
 n=int(input("Input a number to compute the factiorial : "))
 print(factorial(n))
+"""
+"""
+n = 5
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+print(fact(n))
+"""
