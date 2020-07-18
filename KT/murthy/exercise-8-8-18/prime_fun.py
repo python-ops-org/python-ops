81dba2b68e6e91bafceae88f292cddc71fa1f43a
nik786 <chakraborty.rock@gmail.com>
2020-07-18 20:29:09 +0530

    Update prime_fun.py

--- KT/murthy/exercise-8-8-18/prime_fun.py
+++ KT/murthy/exercise-8-8-18/prime_fun.py
@@ -88,5 +88,10 @@ check(n)
 #x = int(raw_input("enter a prime number"))
 #print prime(x)
 
-
+for num in range(2,101):
+    for i in range(2,num):
+        if (num%i==0):
+            break
+    else:
+        print(num)
 
