a6ab80da1fe1b2e8cd3cd59ccd086b51ec4b086d
nik786 <chakraborty.rock@gmail.com>
2020-07-18 20:28:26 +0530

    Update dtob.py

--- KT/murthy/exercise-8-8-18/dtob.py
+++ KT/murthy/exercise-8-8-18/dtob.py
@@ -1,5 +1,20 @@
+
+
 def d(n):
     return bin(n).replace("0b" , "")
 
 if __name__ ==  '__main__':
     print(d(5))
+
+    
+    
+    
+    
+def b(n):
+    return int(n,2)
+
+
+# Driver code
+if __name__ == '__main__':
+    print(b('100')) 
+    
