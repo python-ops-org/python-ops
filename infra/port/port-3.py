ca203c889c9ca420f4ac3e6fac3bbf13812adfef
nik786 <chakraborty.rock@gmail.com>
2020-07-21 23:05:00 +0530

    Create port-3.py

---
+++ infra/port/port-3.py
@@ -0,0 +1,28 @@
+import re
+import subprocess
+from re import findall
+import csv
+from datetime import datetime
+
+def port():
+    try:
+        dt = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        log = open("%s.csv"%dt,"w+")
+        writer=csv.writer(log)
+
+        cmd='netstat -ntlp | grep 80'
+        p=subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+        q=p.stdout.decode("ascii")
+        #print(q)
+        pr = findall(':([^ ]+)', q)[0]
+        #print(pr)
+        #writer.writerow([pr])
+        #if int(pr) == int(80):
+        if int(pr) == None:
+            print("notok")
+        else:
+            print("ok")
+    except:
+        print("notok")
+
+port()
