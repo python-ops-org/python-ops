20cd1d89bc9fb5cb8e96849a6cd97e176a2447bc
nik786 <chakraborty.rock@gmail.com>
2020-08-27 19:17:56 +0530

    Update date.py

--- dates/26-08-2020-local/json/date.py
+++ dates/26-08-2020-local/json/date.py
@@ -4,3 +4,11 @@ yesterday = today - datetime.timedelta(days = 3)
 tomorrow = today + datetime.timedelta(days = 1)
 print(yesterday)
 print(tomorrow)
+
+
+fdate = datetime.datetime.strptime(fdate, '%Y/%m/%d').strftime('%d%m%y')
+print datetime.date.today()
+
+from datetime import date, datetime
+fdate = datetime.strptime(fdate, '%Y/%m/%d').strftime('%d%m%y')
+print date.today()
