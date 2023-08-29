3e96426a0c51f108ec177013c14bcc10a034e002
nik786 <chakraborty.rock@gmail.com>
2023-08-29 21:33:48 +0530

    Update add_large_small_other.py

--- KT/murthy/exercise-8-8-18/add_large_small_other.py
+++ KT/murthy/exercise-8-8-18/add_large_small_other.py
@@ -26,3 +26,23 @@ n1 = np.array([10])
 n2 = np.array([12])
 r = n1 + n2
 print(r)
+
+
+
+n = [1,2,3,4,5]
+n.reverse()
+print(n)
+
+n = 12345
+n = int(str(n)[::-1])
+print(n)
+
+list = [1,2,3,45,6,7,8,9]
+s_list = list[2:6]
+print(s_list)
+
+
+
+
+
+
