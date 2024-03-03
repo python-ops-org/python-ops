08fae06b2b74e36cfa75ee682d0157e5cecb0746
nik786 <chakraborty.rock@gmail.com>
2024-03-03 14:33:29 +0530

    Create class.py

---
+++ KT/murthy/exercise-8-8-18/class.py
@@ -0,0 +1,15 @@
+class Cricket:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+c1 = Cricket("Sachin", 100)
+c2 = Cricket("Rahul", 95)
+
+
+
+
+print(c1.name)
+print(c1.score)
+print(c2.name)
+print(c2.score)
