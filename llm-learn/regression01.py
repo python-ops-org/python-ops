a7b1d25a07a1eb5d0749ea283f2012f6d0bcb0ed
chakraborty.rock@gmail.com <chakraborty.rock@gmail.com>
2024-09-20 09:31:00 +0530

    test

--- llm-learn/regression01.py
+++ llm-learn/regression01.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot as plt
 from sklearn import linear_model
 
 # Load the dataset
-df = pd.read_csv('homes.csv')
+#df = pd.read_csv('homes.csv')
 
+"""
 # Plot the data
 plt.xlabel('Area in sqft')
 plt.ylabel('Price in USD')
@@ -13,3 +14,22 @@ plt.scatter(df.area, df.price, color='red', marker='+')
 
 # Display the plot
 plt.show()
+
+"""
+
+
+df = pd.read_csv('homes.csv')
+new_df = df.drop('price', axis='columns')  # Dropping the 'price' column
+
+area = df[['area']]  # Correct way to extract 'area' column as a DataFrame
+print(type(area))  # Check the type of 'area'
+price = df.price
+print(price)
+
+reg = linear_model.LinearRegression()
+print(reg.fit(area,price))
+
+print(reg.predict([[3300]]))
+
+print(reg.predict([[1240]]))
+
