5410ddffcad491621cc1088472b635362a8b90c2
nik786 <chakraborty.rock@gmail.com>
2024-10-25 21:13:22 +0530

    Update class.py

--- KT/murthy/exercise-8-8-18/class.py
+++ KT/murthy/exercise-8-8-18/class.py
@@ -1,3 +1,10 @@
+
+"""
+Class: A template or blueprint for creating objects (e.g., Dog).
+Constructor (__init__): A special method to initialize object attributes.
+self: Refers to the specific instance of the class, allowing access to instance attributes and methods.
+"""
+
 class Cricket:
     def __init__(self, name, score):
         self.name = name
