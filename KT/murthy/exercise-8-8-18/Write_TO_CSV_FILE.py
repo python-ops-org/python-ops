cd11c7b8b32079349c252cdc4c9759a2a35fb960
nik786 <chakraborty.rock@gmail.com>
2024-11-10 23:36:44 +0530

    Update Write_TO_CSV_FILE.py

--- KT/murthy/exercise-8-8-18/Write_TO_CSV_FILE.py
+++ KT/murthy/exercise-8-8-18/Write_TO_CSV_FILE.py
@@ -8,3 +8,26 @@ writer = csv.writer(file)
 writer.writerow(["NAME","SCORE"])
 data = ["virat", "101"]
 writer.writerow(data)
+
+
+
+
+
+import csv
+
+file = 'test.csv'
+data = [
+    ["name", "score"],
+    ["virat", "100"],
+    ["rohit", "120"]
+]
+
+f = open(file, newline='', mode='w')  # Using your original open syntax
+w = csv.writer(f)
+
+for row in data:
+    w.writerow(row)
+
+f.close()  # Don't forget to close the file
+
+print(f"Data written to {file}")
