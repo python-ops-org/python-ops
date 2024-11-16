9ad5b9ede5dce9bd03be9794d084a07d40104020
nik786 <chakraborty.rock@gmail.com>
2024-11-16 09:29:13 +0530

    Update super-class-inherit.py

--- KT/murthy/exercise-8-8-18/super-class-inherit.py
+++ KT/murthy/exercise-8-8-18/super-class-inherit.py
@@ -10,3 +10,40 @@ class Mumbai(Cricket):
 
 object = Mumbai()
 print(obj.value)
+
+
+
+---------
+
+class Parent:
+    def greet(self):
+        print("Hello from Parent!")
+
+class Child(Parent):
+    def greet(self):
+        super().greet()  # Calls the Parent's greet method
+        print("Hello from Child!")
+
+obj = Child()
+obj.greet()
+
+
+----------
+
+
+class A:
+    def show(self):
+        print("From A")
+
+class B(A):
+    def show(self):
+        print("From B")
+        super().show()
+
+class C(B):
+    def show(self):
+        print("From C")
+        super().show()
+
+obj = C()
+obj.show()
