08bb457529e19daecb22c006350bdecebeb689b4
chakraborty.rock@gmail.com <chakraborty.rock@gmail.com>
2025-07-13 15:20:44 +0530

    adding

--- practise/4.py
+++ practise/4.py
@@ -53,4 +53,18 @@ fibo = [0,1]
 for i in range(5):
     fibo.append(fibo[-1]+fibo[-2])
 print(fibo)
+
+
+def d(n):
+    return bin(n).replace("0b", "")
+if __name__ == '__main__':
+    print(d(5))
+
+
+
+def b(n):
+    return int(n,2)
+if __name__ == '__main__':
+    print(b('100'))
     
+
